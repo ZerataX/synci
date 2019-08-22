8a9c40b25141f12f1279d5bd7033a29433eb93fe
ZerataX <mail@zera.tax>
2019-08-22 20:02:38 +0200

    sync sessions server side

---
+++ synci/session.py
@@ -0,0 +1,40 @@
+from flask import (
+    Blueprint, g, render_template, request
+)
+from synci.auth import login_required
+# from synci.db import get_db
+
+bp = Blueprint('session', __name__, url_prefix='/session')
+sessions = []
+
+
+@login_required()
+@bp.route('/')
+def create():
+    return render_template('session/index.html')
+
+
+@login_required()
+@bp.route('/<session>', methods=('GET', 'POST'))
+def join(session):
+    if request.method == 'POST':
+        for x in sessions:
+            if session == x['name']:
+                session = x
+                break
+        if session and g.user == session['author']:
+            for user in session['followers']:
+                # sync song
+                pass
+    if request.method == 'GET':
+        for x in sessions:
+            if session == x['name']:
+                return render_template('session/session.html')
+        sessions.append({
+            "name": session,
+            "author": g.user,
+            "followers": [],
+            "song": None,
+            "playtime": 0
+        })
+        return render_template('session/session.html')
