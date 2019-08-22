20da5ecaabc051ac0921e445cbdaf296f9b40bbf
ZerataX <mail@zera.tax>
2019-08-22 19:58:52 +0200

    spotify auth server side

---
+++ synci/auth.py
@@ -0,0 +1,53 @@
+import functools
+import urllib.parse
+import random
+
+from flask import (
+    Blueprint, flash, g, redirect, request, session, url_for,
+    current_app
+)
+
+# from synci.db import get_db
+
+SCOPES = "user-read-private%user-read-email"
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register')
+def register():
+    # https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
+    g.state = random.getrandbits(128)
+    auth_url = f"https://accounts.spotify.com/authorize " + \
+               f"?client_id={current_app.config['client_id']}" + \
+               f"&redirect_uri={url_for('auth.register')}" + \
+               f"&scope={urllib.parse.quote(SCOPES)}" + \
+               f"&state={g.state}&response_type=token"
+    return redirect(auth_url)
+
+
+@bp.route('/login')
+def login():
+    error = request.args.get('error')
+    state = request.args.get('state')
+    if state == g.state and not error:
+        session.clear()
+        session['user_id'] = 3  # autoinc user id?
+        return redirect(url_for('index'))
+    flash(error)
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    g.user = session.get('user_id')
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.register'))
+
+        return view(**kwargs)
+
+    return wrapped_view
